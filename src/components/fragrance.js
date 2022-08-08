536e7ef362c7ac3b5b97191478a0d0a51ee3f2af
D1 <D1@corp.techouts.local>
2022-08-08 16:15:17 +0530

    feature/ss6 resolved msite

--- src/components/fragrance.js
+++ src/components/fragrance.js
@@ -2,14 +2,13 @@ import Modelpic from "../assets/images/ambassador_model.png";
 import Premiumpic from "../assets/images/premium_bottle.png";
 import "../assets/styles/frag.css";
 import Swipe from "./swipe.js";
-import MobileModelPic from "../assets/images/ambassador_model_mobile.png"
 
 export default function FragranceStore(){
 
 return(
     <>
         <div className="fragrance">
-            <h2 className="fragrance_topic" >FRAGRANCE STORE</h2><hr style={{marginTop:"28px",marginLeft:"77px",marginRight:"84px",background:"blue"}}/>
+            <h2 className="fragrance_topic" >FRAGRANCE STORE</h2><hr />
             <p className="fragrance_para">Fragrances You Will Love</p>
             <Swipe/>
             <div className="screen2">
@@ -24,7 +23,9 @@ return(
 
             <div  className="screen3">
                 <div className="premium"><h1>Premium Fragrances</h1>
+
                 <p>Planning a Date Night? <span className="invisible"> Keeping things Casual and seemingly spontaneous</span></p><br/>
+
                 <button >VIEW BRANDS</button></div>
                 <img src={Premiumpic} ></img>
             </div>
